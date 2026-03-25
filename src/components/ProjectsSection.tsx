3498c1c393c4aa5c20deb9380cf090986b8f64dd
Najia <najiakayyasa014@gmail.com>
2026-03-25 22:38:43 +0700

    update fitur

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,9 +6,9 @@ import { ExternalLink, Github, Play, ChevronLeft, ChevronRight } from 'lucide-re
 import { Button } from '@/components/ui/button';
 
 // --- 0. IMPORT ASSETS ---
-import BukuImg from '../jifot/buku.png.jpg'; 
-import JoinUsGif from '../jifot/gif.gif';
-import SpiderGif from '../jifot/Spider.gif.gif';
+import BukuImg from '../jiagif/gif.gif'; 
+import JoinUsGif from '../jiagif/gif.gif';
+import SpiderGif from '../jiagif/Spider-Man.gif';
 
 const images = [BukuImg, JoinUsGif, SpiderGif];
 
